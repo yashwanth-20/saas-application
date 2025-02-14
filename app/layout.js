94ce31eed73ffba7d120f605b0ff9a78961c97e9
yashwanth-20 <yashwanthsubramaniyam@gmail.com>
2025-02-14 23:17:48 +0530

    second commit

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {Open_Sans} from 'next/font/google'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const OpenSans=Open_Sans({subsets:['latin']});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={OpenSans.className}
       >
         {children}
       </body>
